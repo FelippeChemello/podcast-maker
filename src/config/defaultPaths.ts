8c6561654265924577ec54d2744c96785ef24800
Felippe Chemello <felippechemello@gmail.com>
2022-02-05 04:02:25 -0300

    Add getPath logs

--- src/config/defaultPaths.ts
+++ src/config/defaultPaths.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { Config } from '@oclif/core';
 
+import { log } from '../utils/log';
+
 const assetsPath = path.resolve(__dirname, '..', '..', 'assets');
 const remotionPath = path.resolve(__dirname, '..', '..', 'video');
 
@@ -13,23 +15,39 @@ export const getPath = async (
     switch (pathname) {
         case 'content':
             const contentPath = path.resolve(config.dataDir, 'content');
+            log(`Content path: ${contentPath}`, 'getPath');
 
             if (!fs.existsSync(contentPath)) {
+                log(
+                    `Content path doesn't exist: ${contentPath}. Creating...`,
+                    'getPath',
+                );
+
                 fs.mkdirSync(contentPath, { recursive: true });
             }
 
             return contentPath;
         case 'assets':
+            log(`Assets path: ${assetsPath}`, 'getPath');
+
             return assetsPath;
         case 'tmp':
             const tmpPath = path.resolve(config.cacheDir, 'tmp');
+            log(`Tmp path: ${tmpPath}`, 'getPath');
 
             if (!fs.existsSync(tmpPath)) {
+                log(
+                    `Tmp path doesn't exist: ${tmpPath}. Creating...`,
+                    'getPath',
+                );
+
                 fs.mkdirSync(tmpPath, { recursive: true });
             }
 
             return tmpPath;
         case 'remotion':
+            log(`Remotion path: ${remotionPath}`, 'getPath');
+
             return remotionPath;
         default:
             throw new Error(`Unknown path: ${pathname}`);
