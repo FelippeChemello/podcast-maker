b79f5b0a94d61d6e774cf3efcdb657fd911b0527
FelippeChemello <felippechemello@gmail.com>
2023-03-06 03:17:52 -0300

    fix(paths)

---
+++ src/hooks/prerun/copyLastContentToPublic.ts
@@ -0,0 +1,25 @@
+import { Hook } from '@oclif/core'
+import { getPath } from 'config/defaultPaths';
+import fs from 'fs'
+import path from 'path'
+
+import { getLatestFileCreated } from '../../utils/getFiles';
+
+const hook: Hook<'prerun'> = async function (opts) {
+    if (opts.Command.name !== 'Create') {
+        return;
+    }
+
+    console.log("@@@")
+
+    const lastContentFile = await getLatestFileCreated('json', path.resolve(__dirname, '..', '..', '..', 'content'))
+    const contentFileDestination = path.resolve(await getPath('content'), path.basename(lastContentFile))
+
+    console.log(contentFileDestination)
+
+    fs.copyFileSync(lastContentFile, contentFileDestination)
+
+    await new Promise((resolve) => { setTimeout(resolve, 5000) })
+}
+
+export default hook
