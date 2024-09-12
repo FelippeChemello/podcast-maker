531ce6efad1d73f65939df6db30c3b1fa88192c0
FelippeChemello <felippechemello@gmail.com>
2024-09-12 20:45:18 -0300

    feat: text fit inside screen and refactor on audio visualization

--- video/src/Podcast/AudioWaveform.tsx
+++ video/src/Podcast/AudioWaveform.tsx
@@ -1,106 +1,62 @@
 import React, {useEffect, useState} from 'react';
 import {interpolate, useCurrentFrame, useVideoConfig, Audio, staticFile} from 'remotion';
-import {getAudioData} from '@remotion/media-utils';
+import {getAudioData, useAudioData, visualizeAudio} from '@remotion/media-utils';
 
 export const AudioWaveform: React.FC<{
 	audioFilePath: string;
 }> = ({audioFilePath}) => {
-	const {width: videoWidth, durationInFrames} = useVideoConfig();
 	const frame = useCurrentFrame();
-
-	const BAR_WIDTH = 7;
-	const BAR_MARGIN_BETWEEN = 2;
-	const MAX_BAR_HEIGHT = 100;
-	const QUANTITY_OF_SAMPLES = durationInFrames / 1.5;
+    const { fps } = useVideoConfig();
 
 	const audioSrc = staticFile(audioFilePath.substring(audioFilePath.lastIndexOf('/') + 1))
-	const [waveforms, setWaveforms] = useState<number[] | null>(null);
+    const audioData = useAudioData(audioSrc);
 
-	useEffect(() => {
-        getAudioData(audioSrc).then((audioData) => {
+    if (!audioData) {
+        return null;
+    }
 
-            const fullWaveforms = Array.from(audioData.channelWaveforms[0]);
+    const allVisualizationValues = visualizeAudio({
+        fps,
+        frame,
+        audioData,
+        numberOfSamples: 256,
+    });
 
-            const blockSize = Math.floor(
-                fullWaveforms.length / QUANTITY_OF_SAMPLES
-            );
+    const visualization = allVisualizationValues.slice(12, 150);
 
-            const blocks = new Array(
-                Math.floor(fullWaveforms.length / blockSize)
-            )
-                .fill(0)
-                .map((_) => fullWaveforms.splice(0, blockSize));
+    const mirrored = [...visualization.slice(1).reverse(), ...visualization];
 
-            const waveformValues = blocks
-                .map(
-                    (block) =>
-                        block.reduce(
-                            (accumulator, value) =>
-                                accumulator + Math.abs(value),
-                            0
-                        ) / block.length
-                )
-                .filter((value) => !!value);
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                height: '10vh',
+                alignItems: 'flex-end',
+                justifyContent: 'center',
+                gap: '1px',
+                position: 'fixed',
+                bottom: 0,
+                padding: '0px 8px',
+                zIndex: 75,
+            }}
+        >
+            {mirrored.map((v, index) => {
+                const height = Math.abs(500 * Math.sqrt(v));
 
-            const multiplier = Math.pow(Math.max(...waveformValues), -1);
-
-            const smoothWaveforms = waveformValues.map(
-                (value, index, array) => {
-                    const weightOfMean = 5;
-                    const toSmooth = [
-                        array[index - 1],
-                        value * 3,
-                        array[index + 1],
-                    ];
-
-                    const smootherValue =
-                        toSmooth.reduce((acc, value) => acc + value, 0) /
-                        weightOfMean;
-
-                    return smootherValue * multiplier;
-                }
-            );
-
-            setWaveforms(smoothWaveforms);
-        })
-	}, [audioFilePath]);
-
-	if (!waveforms || !audioSrc) {
-		return null;
-	}
-
-	const position = interpolate(frame, [10, durationInFrames - 20], [0, 100]);
-
-	return (
-		<>
-			<Audio src={audioSrc} />
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					alignItems: 'center',
-					height: MAX_BAR_HEIGHT + 50,
-					width: waveforms.length * (BAR_WIDTH + BAR_MARGIN_BETWEEN),
-					transform: `translateX(calc(${videoWidth}px - ${position}%))`,
-				}}
-			>
-				{waveforms.map((v, i) => {
-					const height = MAX_BAR_HEIGHT * v;
-
-					return (
-						<div
-							key={i}
-							style={{
-								height,
-								width: BAR_WIDTH,
-								backgroundColor: '#3997db',
-								marginLeft: BAR_MARGIN_BETWEEN,
-								borderRadius: 10,
-							}}
-						/>
-					);
-				})}
-			</div>
-		</>
-	);
+                return (
+                    <div
+                        key={`AudioVisualization-${index}`}
+                        style={{
+                            height: `${height > 1 ? height : 0}%`,
+                            maxHeight: '64px',
+                            width: '6px',
+                            background: '#D7DADC',
+                            borderRadius: '4px',
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
 };
