103958b13e13cbc7d2eca901020e606e77aa5e0b
Felippe Chemello <felippechemello@gmail.com>
2024-09-13 13:00:33 -0300

    fix audio issue

--- video/src/Podcast/AudioWaveform.tsx
+++ video/src/Podcast/AudioWaveform.tsx
@@ -27,36 +27,39 @@ export const AudioWaveform: React.FC<{
     const mirrored = [...visualization.slice(1).reverse(), ...visualization];
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                height: '10vh',
-                alignItems: 'flex-end',
-                justifyContent: 'center',
-                gap: '1px',
-                position: 'fixed',
-                bottom: 0,
-                padding: '0px 8px',
-                zIndex: 75,
-            }}
-        >
-            {mirrored.map((v, index) => {
-                const height = Math.abs(500 * Math.sqrt(v));
+        <>
+            <Audio src={audioSrc} />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    height: '10vh',
+                    alignItems: 'flex-end',
+                    justifyContent: 'center',
+                    gap: '1px',
+                    position: 'fixed',
+                    bottom: 0,
+                    padding: '0px 8px',
+                    zIndex: 75,
+                }}
+            >
+                {mirrored.map((v, index) => {
+                    const height = Math.abs(500 * Math.sqrt(v));
 
-                return (
-                    <div
-                        key={`AudioVisualization-${index}`}
-                        style={{
-                            height: `${height > 1 ? height : 0}%`,
-                            maxHeight: '64px',
-                            width: '6px',
-                            background: '#D7DADC',
-                            borderRadius: '4px',
-                        }}
-                    />
-                );
-            })}
-        </div>
+                    return (
+                        <div
+                            key={`AudioVisualization-${index}`}
+                            style={{
+                                height: `${height > 1 ? height : 0}%`,
+                                maxHeight: '64px',
+                                width: '6px',
+                                background: '#D7DADC',
+                                borderRadius: '4px',
+                            }}
+                        />
+                    );
+                })}
+            </div>
+        </>
     );
 };
